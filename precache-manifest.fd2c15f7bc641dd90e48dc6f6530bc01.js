b04721a0ca11f1f05ce16f9b2ab4d7b3308c97f2
Sarthak Awasthi <sa36655@drexel.edu>
2020-05-10 20:01:41 -0700

    Portfolio Website

---
+++ precache-manifest.fd2c15f7bc641dd90e48dc6f6530bc01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c0f7d309a0f9deb6802d434b0192b59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7c83c367da7803cb7cf",
+    "url": "/static/css/main.76ff40f4.chunk.css"
+  },
+  {
+    "revision": "d52f2ff11baff8480437",
+    "url": "/static/js/2.ff031452.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff031452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7c83c367da7803cb7cf",
+    "url": "/static/js/main.55303e8a.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  }
+]);
